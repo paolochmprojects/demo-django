b2a3843d10889083db797d1e10521480d4207296
paolochmprojects <paolo.dev.projects@gmail.com>
2025-01-16 07:28:05 -0500

    Initialize Django project with task management app structure and basic configurations

---
+++ task/models.py
@@ -0,0 +1,16 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tasks', null=True)
+
+    def __str__(self):
+        return self.title
