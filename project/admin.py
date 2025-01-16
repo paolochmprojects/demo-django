e3a1d21f147841332a0a836b303fa28bffbadb16
paolochmprojects <paolo.dev.projects@gmail.com>
2025-01-16 15:02:03 -0500

    [ADD] update

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+CustomUser = get_user_model()
+
+admin.site.register(Project)
+admin.site.register(CustomUser)
